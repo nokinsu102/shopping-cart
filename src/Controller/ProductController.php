317fe9b02f062c59bd64659216b65207278692ca
Nokinsu <mjb102denhaag@gmail.com>
2019-09-03 22:05:44 +0200

    first commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+/**
+ * @Route("/product")
+ */
+class ProductController extends AbstractController
+{
+
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @Route("/", name="product_index", methods={"GET"})
+     */
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="product_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="product_show", methods={"GET"})
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="product_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Product $product): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="product_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Product $product): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($product);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('product_index');
+    }
+
+    /**
+     * @Route("/cart/{id}", name="product_cart", methods={"GET", "POST"})
+     */
+    public function addToCart(Product $product, $id)
+     {
+        $getCart = $this->session->get('cart', []);
+
+        if(isset($getCart[$id])){
+            $getCart[$id]['amount']++;
+        }
+        else{
+            $getCart[$id] = array(
+                'amount' => 1,
+                'name' =>$product-> getName(),
+                'price' => $product->getImage(),
+                'id' =>$product->getId(),
+            );
+        }
+        $this->session->set('cart', $getCart);
+        return $this->render('product/cart.html.twig',[
+       'product' => $getCart[$id]['name'],
+       'amount' => $getCart[$id]['amount'],
+       'cart' => $getCart
+            ]);
+    }
+}
+
